657dab9baa1dfbaf3c2bb49b7a8676d634e06b99
Daniel Agbogun <daniel.agbogun@globalaccelerex.com>
2020-09-19 10:43:34 +0100

    Sign-in page

--- src/app/pages/landing-page/authentication/sign-in/sign-in.component.ts
+++ src/app/pages/landing-page/authentication/sign-in/sign-in.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
-
-  constructor() { }
+  loginForm: FormGroup;
+  validationMessage: any;
+  isLoading: boolean;
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.initializeForm();
   }
 
+  initializeForm() {
+    this.loginForm = this.formBuilder.group({
+      email: [''],
+      password: [''],
+    });
+  }
 }
