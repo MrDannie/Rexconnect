a2fd3ed151ad8ee286a46a724a2952f478c00bc9
Daniel Agbogun <daniel.agbogun@globalaccelerex.com>
2020-09-20 21:32:05 +0100

    The Terminal Page UI

--- src/app/pages/user/user-layout/terminals/terminals.component.ts
+++ src/app/pages/user/user-layout/terminals/terminals.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-terminals',
   templateUrl: './terminals.component.html',
-  styleUrls: ['./terminals.component.scss']
+  styleUrls: ['./terminals.component.scss'],
 })
 export class TerminalsComponent implements OnInit {
+  showFilter: boolean;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.showFilter = false;
   }
 
+  ngOnInit() {}
+
+  reset() {}
+
+  generateCSV() {}
 }
