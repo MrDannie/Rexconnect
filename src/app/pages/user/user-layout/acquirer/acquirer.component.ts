cd69c8013dde9409b7b0555c522f7d08f26d6c23
Daniel Agbogun <daniel.agbogun@globalaccelerex.com>
2020-09-21 16:26:25 +0100

    Acquirer Modal

--- src/app/pages/user/user-layout/acquirer/acquirer.component.ts
+++ src/app/pages/user/user-layout/acquirer/acquirer.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-acquirer',
   templateUrl: './acquirer.component.html',
-  styleUrls: ['./acquirer.component.scss']
+  styleUrls: ['./acquirer.component.scss'],
 })
 export class AcquirerComponent implements OnInit {
+  createAcquirerForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
+    this.initializeForm();
   }
 
+  ngOnInit() {}
+
+  initializeForm() {
+    // this.searchForm = this.formBuilder.group({
+    //   firstname: "",
+    //   lastname: "",
+    //   role: "",
+    // });
+    this.createAcquirerForm = this.formBuilder.group({
+      merchantId: ['', Validators.compose([Validators.required])],
+      terminalId: ['', Validators.compose([Validators.required])],
+    });
+  }
 }
