f863dfda479c310096093d04336d683f581a7dac
Daniel Agbogun <daniel.agbogun@globalaccelerex.com>
2020-09-21 21:32:39 +0100

    Stations UI

--- src/app/pages/user/user-layout/stations/stations.component.ts
+++ src/app/pages/user/user-layout/stations/stations.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-stations',
   templateUrl: './stations.component.html',
-  styleUrls: ['./stations.component.scss']
+  styleUrls: ['./stations.component.scss'],
 })
 export class StationsComponent implements OnInit {
+  createStationForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
+    this.initializeForm();
   }
+  ngOnInit() {}
 
+  initializeForm() {
+    // this.searchForm = this.formBuilder.group({
+    //   firstname: "",
+    //   lastname: "",
+    //   role: "",
+    // });
+    this.createStationForm = this.formBuilder.group({
+      merchantId: ['', Validators.compose([Validators.required])],
+      terminalId: ['', Validators.compose([Validators.required])],
+    });
+  }
 }
