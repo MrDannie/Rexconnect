b40fb8967f0ccaf05e7bac18d1ba150949b4a3bb
Daniel Agbogun <daniel.agbogun@globalaccelerex.com>
2020-09-21 15:10:16 +0100

    terminal UI

--- src/app/pages/user/user-layout/terminals/terminals.component.ts
+++ src/app/pages/user/user-layout/terminals/terminals.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-terminals',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TerminalsComponent implements OnInit {
   showFilter: boolean;
+  createTerminalForm: FormGroup;
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
     this.showFilter = false;
+    this.initializeForm();
   }
 
   ngOnInit() {}
@@ -17,4 +20,16 @@ export class TerminalsComponent implements OnInit {
   reset() {}
 
   generateCSV() {}
+
+  initializeForm() {
+    // this.searchForm = this.formBuilder.group({
+    //   firstname: "",
+    //   lastname: "",
+    //   role: "",
+    // });
+    this.createTerminalForm = this.formBuilder.group({
+      merchantId: ['', Validators.compose([Validators.required])],
+      terminalId: ['', Validators.compose([Validators.required])],
+    });
+  }
 }
