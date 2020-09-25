c4feed31d6e54336489127c8ebda0faed3f7f53a
Daniel Agbogun <daniel.agbogun@globalaccelerex.com>
2020-09-25 08:51:41 +0100

    some changes

--- src/app/pages/user/user-layout/user-management/user-management-routing.module.ts
+++ src/app/pages/user/user-layout/user-management/user-management-routing.module.ts
@@ -4,19 +4,15 @@ import { EditUserComponent } from './edit-user/edit-user.component';
 import { ManageUserComponent } from './manage-user/manage-user.component';
 import { RoleManagementComponent } from './role-management/role-management.component';
 
-// const routes: Routes = [
-
-// ];
+const routes: Routes = [
+  { path: '', redirectTo: 'all-users' },
+  { path: 'all-users', component: ManageUserComponent },
+  { path: 'manage-roles', component: RoleManagementComponent },
+  { path: 'edit-user/:id', component: EditUserComponent },
+];
 
 @NgModule({
-  imports: [
-    RouterModule.forChild([
-      { path: '', pathMatch: 'prefix', redirectTo: 'all-users' },
-      { path: 'all-users', component: ManageUserComponent },
-      { path: 'manage-roles', component: RoleManagementComponent },
-      { path: 'edit-user/:id', component: EditUserComponent },
-    ]),
-  ],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
 export class UserManagementRoutingModule {}
