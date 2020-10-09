295f1acf84d96845bffd80072fcd7be5d89cf159
Daniel Agbogun <daniel.agbogun@globalaccelerex.com>
2020-10-09 21:38:03 +0100

    Update on Terminals UI Design

---
+++ src/app/pages/user/user-layout/manage-terminal/manage-terminal-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ManageTerminalComponent } from './manage-terminal/manage-terminal.component';
+import { TerminalDetailsComponent } from './terminal-details/terminal-details.component';
+import { TerminalTransactionsComponent } from './terminal-transactions/terminal-transactions.component';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild([
+      {
+        path: '',
+        component: ManageTerminalComponent,
+        children: [
+          {
+            path: '',
+            redirectTo: 'terminal-details',
+            pathMatch: 'full',
+          },
+          {
+            path: 'terminal-details',
+            component: TerminalDetailsComponent,
+          },
+          {
+            path: 'terminal-transaction',
+            component: TerminalTransactionsComponent,
+          },
+        ],
+      },
+    ]),
+  ],
+  exports: [RouterModule],
+})
+export class ManageTerminalRoutingModule {}
