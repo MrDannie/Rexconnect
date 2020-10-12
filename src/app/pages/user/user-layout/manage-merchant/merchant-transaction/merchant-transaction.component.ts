a78a097e4036f21583798a35fd6f838eba5fa5e7
Daniel Agbogun <daniel.agbogun@globalaccelerex.com>
2020-10-12 17:02:08 +0100

    changes on merchant mgt module

--- src/app/pages/user/user-layout/manage-merchant/merchant-transaction/merchant-transaction.component.ts
+++ src/app/pages/user/user-layout/manage-merchant/merchant-transaction/merchant-transaction.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-merchant-transaction',
@@ -6,11 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./merchant-transaction.component.scss'],
 })
 export class MerchantTransactionComponent implements OnInit {
+  showFilter: boolean;
+  expression: boolean;
+  isCSVLoading;
+  boolean;
+
   ngForArray: number[];
+  editAcquirerForm: any;
 
-  constructor() {}
-
-  ngOnInit() {
-    this.ngForArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11];
+  constructor(private formBuilder: FormBuilder) {
+    this.showFilter = false;
+    this.expression = false;
+    this.isCSVLoading = false;
+    this.initializeForm();
+    this.ngForArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   }
+
+  ngOnInit() {}
+
+  initializeForm() {
+    this.editAcquirerForm = this.formBuilder.group({
+      merchantId: ['', Validators.compose([Validators.required])],
+      terminalId: ['', Validators.compose([Validators.required])],
+    });
+  }
+
+  reset() {}
+  generateCSV() {}
+
+  createUser(value) {}
 }
