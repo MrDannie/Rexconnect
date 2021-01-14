44f7cf91d6fe8d13bfcde225d0eaf7dcd7f1bd42
Daniel Agbogun <daniel.agbogun@globalaccelerex.com>
2021-01-14 10:08:04 +0100

    Some changes

---
+++ src/app/pages/user/user-layout/manage-routes/edit-routes/edit-routes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-routes',
+  templateUrl: './edit-routes.component.html',
+  styleUrls: ['./edit-routes.component.scss']
+})
+export class EditRoutesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pages/user/user-layout/manage-routes/manage-routes-routing.module.ts
+++ src/app/pages/user/user-layout/manage-routes/manage-routes-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddRoutesComponent } from './add-routes/add-routes.component';
+import { EditRoutesComponent } from './edit-routes/edit-routes.component';
 import { RoutesDetailsComponent } from './routes-details/routes-details.component';
 
 import { RoutesComponent } from './routes/routes.component';
@@ -22,10 +23,10 @@ const routes: Routes = [{ path: 'routePath', component: RoutesComponent }];
         path: 'add-routes/:id',
         component: AddRoutesComponent,
       },
-      // {
-      //   path: 'edit-routes/:id',
-      //   component: EditRoutesComponent,
-      // },
+      {
+        path: 'edit-routes/:id',
+        component: EditRoutesComponent,
+      },
     ]),
   ],
   exports: [RouterModule],
