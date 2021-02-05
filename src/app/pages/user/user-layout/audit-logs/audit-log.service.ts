7f08e635970233c1b700cbade3d78a24df06c0a6
Rahman Hafeez <rahman.hafeez@globalaccelerex.com>
2021-02-05 09:08:41 +0100

    audit logs and design fixes

--- src/app/pages/user/user-layout/audit-logs/audit-log.service.ts
+++ src/app/pages/user/user-layout/audit-logs/audit-log.service.ts
@@ -3,56 +3,58 @@ import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Config } from 'src/app/core/Config';
 import { StorageService } from 'src/app/core/helpers/storage.service';
+import { DateService } from 'src/app/pages/shared/services/date.service';
 import { environment } from 'src/environments/environment';
-
-
-
-
 
 const BASE_URL = environment.EXTERNAL_BASE_URL;
 const APP_REF = environment.APP_REF;
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuditLogService {
 
   constructor(
     private httpClient: HttpClient,
     private storageService: StorageService,
-    private config: Config
+    private config: Config,
+    private dateService: DateService,
   ) { }
 
+  public getAuditLogs(pageIndex, pageSize, searchParams?) {
 
-
-  getAuditLogs(pageIndex, pageSize, searchFormValue?) {
-    
-    const {clientId} = this.storageService.getCurrentUser()['user'];
+    const {clientId} = this.storageService.getCurrentUser().user;
     let params = new HttpParams();
 
     if (pageIndex) {
-      params = params.append("offset", pageIndex);
+      params = params.append('offset', pageIndex);
     }
     if (pageSize) {
-      params = params.append("limit", pageSize);
+      params = params.append('limit', pageSize);
     }
-    if (searchFormValue) {
-      if (searchFormValue.name) {
-        params = params.append("name", searchFormValue.name);
+    if (searchParams) {
+
+      if (searchParams.startDate) {
+        params = params.append(
+          'start',
+          this.dateService.formatDate(searchParams.startDate),
+        );
       }
-      if (searchFormValue.status) {
-        params = params.append("isActive", searchFormValue.status);
+
+      if (searchParams.endDate) {
+        params = params.append(
+          'end',
+          this.dateService.formatDate(searchParams.endDate)
+        )
       }
     }
-   
 
     return this.httpClient
       .get(`${BASE_URL}${this.config.auditLogs}${APP_REF}${clientId}` , { params })
       .pipe(
-        map((stations) => {
-          return stations;
-        })
+        map((resp) => {
+          return resp;
+        }),
       );
   }
 
