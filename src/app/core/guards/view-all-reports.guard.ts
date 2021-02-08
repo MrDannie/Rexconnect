b4235290f20ed11af5ed2476aa0419bf8ca05716
Daniel Agbogun <daniel.agbogun@gmail.com>
2021-02-08 16:23:40 +0100

    feature/RK-276-route-guard-to-routes

---
+++ src/app/core/guards/view-all-reports.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { StorageService } from '../helpers/storage.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ViewAllReportsGuard implements CanActivate {
+  constructor(private storageService: StorageService, private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean | Observable<boolean> | Promise<boolean> {
+    const permissions = this.storageService.getPermissions();
+
+    if (permissions.includes('CAN_VIEW_ALL_REPORT')) {
+      console.log('IT REACHED THE AUTH GUARD');
+      return true;
+    } else {
+      this.router.navigate(['sign-in']);
+      return false;
+    }
+  }
+}
