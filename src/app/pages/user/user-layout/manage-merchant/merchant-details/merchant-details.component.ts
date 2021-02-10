f9b0e75479dc05b6b559dc38775bb0e019b74df5
Daniel Agbogun <daniel.agbogun@gmail.com>
2021-02-10 11:22:49 +0100

    hotfix/RK-280-include-status-column-to-merchant-and-terminal-column

--- src/app/pages/user/user-layout/manage-merchant/merchant-details/merchant-details.component.ts
+++ src/app/pages/user/user-layout/manage-merchant/merchant-details/merchant-details.component.ts
@@ -9,6 +9,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 // tslint:disable
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { StorageService } from 'src/app/core/helpers/storage.service';
 
 @Component({
   selector: 'app-merchant-details',
@@ -29,16 +30,23 @@ export class MerchantDetailsComponent implements OnInit {
   currencyCodes: any[];
   countryCodes: any[];
   allCities: any[] = [];
-  storedCities: { country_code: string; subdivision_name: string; code: string; }[];
+  storedCities: {
+    country_code: string;
+    subdivision_name: string;
+    code: string;
+  }[];
 
   citiesMap = new Map();
+  allTimeZones: any;
+  autoMidState: any;
 
   constructor(
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private merchants: MerchantsService,
     private alerts: AlertService,
-    private errorHandler: ErrorHandler
+    private errorHandler: ErrorHandler,
+    private storageService: StorageService
   ) {
     this.initializeForm();
   }
@@ -48,20 +56,27 @@ export class MerchantDetailsComponent implements OnInit {
     this.merchants.getMerchant(this.merchantId).subscribe(
       (data) => {
         this.merchantDetails = data;
+        console.log('THIS IS MERCHNT DETAILS', this.merchantDetails);
+
         this.populateEditForm();
         // forward terminal loading to next method
         this.getMerchantTerminals();
       },
-      error => {
+      (error) => {
         this.isLoadingTerminals = false;
-        this.errorHandler.customClientErrors('Error occurred when getting merchant details',
+        this.errorHandler.customClientErrors(
+          'Error occurred when getting merchant details',
           error.error.error.code,
           error.error.error.responseMessage
         );
       }
-    )
+    );
   }
 
+  getAutoMidState() {
+    this.autoMidState = this.storageService.getCurrentUser().user.autoMID;
+    console.log('AUTO MID STATE', this.autoMidState);
+  }
   updateMerchant() {
     this.isUpdatingMerchant = true;
     const updatedMerchant = {
@@ -72,25 +87,42 @@ export class MerchantDetailsComponent implements OnInit {
       merchantKey: this.updateMerchantForm.value.merchantKey,
       currencyCode: this.updateMerchantForm.value.currency,
       countryCode: Number(this.updateMerchantForm.value.countryCode),
-      city: this.updateMerchantForm.value.city
-    }
-    console.log(updatedMerchant)
-    this.merchants.updateMerchant(this.merchantId, updatedMerchant)
-      .subscribe(
-        response => {
-          this.isUpdatingMerchant = false;
-          this.closeModal('cancel_button_edit_merchant');
-          this.alerts.success('Merchant updated successfully!');
-          this.getMerchantDetails();
-        },
-        error => {
-          this.isUpdatingMerchant = false;
-          this.errorHandler.customClientErrors('Error occurred while creating merchant',
+      city: this.updateMerchantForm.value.city,
+      timezoneId: +this.updateMerchantForm.value.timezoneId,
+    };
+    console.log(updatedMerchant);
+    this.merchants.updateMerchant(this.merchantId, updatedMerchant).subscribe(
+      (response) => {
+        this.isUpdatingMerchant = false;
+        this.closeModal('cancel_button_edit_merchant');
+        this.alerts.success('Merchant updated successfully!');
+        this.getMerchantDetails();
+      },
+      (error) => {
+        this.isUpdatingMerchant = false;
+        this.errorHandler.customClientErrors(
+          'Error occurred while creating merchant',
           error.error.error.code,
           error.error.error.responseMessage
         );
-        }
-      )
+      }
+    );
+  }
+
+  getMerchantTimezones() {
+    const timeZones = this.storageService.getTimezones();
+    if (timeZones) {
+      // dont fetch data
+      console.log('RECSDEFDJFFHFHHFHFHFH');
+
+      this.allTimeZones = timeZones;
+    } else {
+      this.merchants.getTimezones().subscribe((response) => {
+        console.log('THese are the time zones', response);
+        this.allTimeZones = response['data'];
+        this.storageService.storeTimeZones(response.data);
+      });
+    }
   }
 
   closeModal(id) {
@@ -99,47 +131,55 @@ export class MerchantDetailsComponent implements OnInit {
 
   getMerchantTerminals() {
     this.isLoadingTerminals = true;
-    this.merchants.getMerchantTerminals(this.merchantDetails.merchantId).subscribe(
-      data => {
-        this.merchantTerminals = data.content;
-        this.isLoadingTerminals = false;
-      },
-      error => {
-        this.isLoadingTerminals = false;
-        this.errorHandler.customClientErrors('Error occurred while getting merchant terminals',
-          error.error.error.code,
-          error.error.error.responseMessage
-        );
-      }
-    )
+    this.merchants
+      .getMerchantTerminals(this.merchantDetails.merchantId)
+      .subscribe(
+        (data) => {
+          this.merchantTerminals = data.content;
+          this.isLoadingTerminals = false;
+        },
+        (error) => {
+          this.isLoadingTerminals = false;
+          this.errorHandler.customClientErrors(
+            'Error occurred while getting merchant terminals',
+            error.error.error.code,
+            error.error.error.responseMessage
+          );
+        }
+      );
   }
 
   getCurrencyName(code) {
-    const currencyName = currencies.CURRENCY_CODES.find(curr => {
-      return curr["ISO4217-currency_numeric_code"] == code;
-    })["ISO4217-currency_name"];
+    const currencyName = currencies.CURRENCY_CODES.find((curr) => {
+      return curr['ISO4217-currency_numeric_code'] == code;
+    })['ISO4217-currency_name'];
     return currencyName;
   }
 
   getCountryName(code) {
-    const countryName = countries.COUNTRY_CODES.find(country => {
-      return country["ISO3166-1-numeric"] == code;
-    })["ISO3166-1-Alpha-3"];
+    const countryName = countries.COUNTRY_CODES.find((country) => {
+      return country['ISO3166-1-numeric'] == code;
+    })['ISO3166-1-Alpha-3'];
     return countryName;
   }
 
   getMCCodes(code) {
-    const mcc = merchantCodes.MERCHANT_CODES.find(merchant => {
-      return merchant["MCC CODE"] == code;
-    })["Program Type"];
+    const mcc = merchantCodes.MERCHANT_CODES.find((merchant) => {
+      return merchant['MCC CODE'] == code;
+    })['Program Type'];
     return mcc;
   }
 
   // utils
   getCountries() {
     this.countryCodes = countries.COUNTRY_CODES;
-    this.countryCodes = this.countryCodes.map(function(country) {
-      country.fullCountryLabel = country['ISO3166-1-Alpha-3'] + ' ' + '(' + country['ISO3166-1-numeric'] + ')';
+    this.countryCodes = this.countryCodes.map(function (country) {
+      country.fullCountryLabel =
+        country['ISO3166-1-Alpha-3'] +
+        ' ' +
+        '(' +
+        country['ISO3166-1-numeric'] +
+        ')';
       return country;
     });
   }
@@ -147,7 +187,8 @@ export class MerchantDetailsComponent implements OnInit {
   getCategoryCodes() {
     this.merchantCategoryCodes = merchantCodes.MERCHANT_CODES;
     this.merchantCategoryCodes = this.merchantCategoryCodes.map(function (cat) {
-      cat.fullCatLabel = cat['Program Type'] + ' ' + '(' + cat['MCC CODE'] + ')';
+      cat.fullCatLabel =
+        cat['Program Type'] + ' ' + '(' + cat['MCC CODE'] + ')';
       return cat;
     });
   }
@@ -155,16 +196,21 @@ export class MerchantDetailsComponent implements OnInit {
   getCurrencyCodes() {
     this.currencyCodes = currencies.CURRENCY_CODES;
     this.currencyCodes = this.currencyCodes.map(function (curr) {
-      curr.fullCurrencyLabel = curr['ISO4217-currency_name'] + ' ' + '(' + curr['ISO4217-currency_numeric_code'] + ')';
+      curr.fullCurrencyLabel =
+        curr['ISO4217-currency_name'] +
+        ' ' +
+        '(' +
+        curr['ISO4217-currency_numeric_code'] +
+        ')';
       return curr;
     });
   }
 
   onSelectCountryCode(countryCode) {
-    const countryObj = countries.COUNTRY_CODES.find(country => {
-      return country["ISO3166-1-numeric"] == countryCode;
-    })
-    const countryAlpha2 = countryObj["ISO3166-1-Alpha-2"];
+    const countryObj = countries.COUNTRY_CODES.find((country) => {
+      return country['ISO3166-1-numeric'] == countryCode;
+    });
+    const countryAlpha2 = countryObj['ISO3166-1-Alpha-2'];
     this.getAllCities(countryAlpha2);
   }
 
@@ -181,30 +227,30 @@ export class MerchantDetailsComponent implements OnInit {
     this.route.params.subscribe((params: Params) => {
       this.merchantId = params.id;
       this.getMerchantDetails();
-    })
+    });
     this.storedCities = states.STATES;
     this.getCategoryCodes();
     this.getCountries();
     this.getCurrencyCodes();
-
+    this.getMerchantTimezones();
+    this.getAutoMidState();
   }
 
   populateEditForm() {
-
     let actualCountryCode;
     const countryCode = this.merchantDetails.countryCode;
     if (isNaN(Number(countryCode))) {
-      const countryObj = countries.COUNTRY_CODES.find(country => {
-        return country["ISO3166-1-Alpha-2"] == countryCode;
-      })
-      actualCountryCode = countryObj["ISO3166-1-numeric"];
+      const countryObj = countries.COUNTRY_CODES.find((country) => {
+        return country['ISO3166-1-Alpha-2'] == countryCode;
+      });
+      actualCountryCode = countryObj['ISO3166-1-numeric'];
       this.getAllCities(countryCode);
     } else {
       actualCountryCode = countryCode;
-      const countryObj = countries.COUNTRY_CODES.find(country => {
-        return country["ISO3166-1-numeric"] == Number(countryCode);
-      })
-      this.getAllCities(countryObj["ISO3166-1-Alpha-2"]);
+      const countryObj = countries.COUNTRY_CODES.find((country) => {
+        return country['ISO3166-1-numeric'] == Number(countryCode);
+      });
+      this.getAllCities(countryObj['ISO3166-1-Alpha-2']);
     }
 
     this.updateMerchantForm.patchValue({
@@ -215,7 +261,8 @@ export class MerchantDetailsComponent implements OnInit {
       categoryCode: this.merchantDetails.merchantCategoryCode,
       countryCode: actualCountryCode,
       city: this.merchantDetails.city,
-      merchantToken: this.merchantDetails.merchantToken
+      merchantToken: this.merchantDetails.merchantToken,
+      timezoneId: +this.merchantDetails.timezoneId,
     });
   }
 
@@ -223,15 +270,19 @@ export class MerchantDetailsComponent implements OnInit {
     this.updateMerchantForm = this.formBuilder.group({
       merchantName: ['', Validators.required],
       merchantKey: ['', Validators.required],
-      merchantId: ['',
-        Validators.compose([Validators.required, Validators.maxLength(15), Validators.minLength(15)])
+      merchantId: [
+        '',
+        Validators.compose([
+          Validators.maxLength(15),
+          Validators.minLength(15),
+        ]),
       ],
       currency: ['', Validators.required],
       categoryCode: ['', Validators.required],
       countryCode: ['', Validators.required],
       city: ['', Validators.required],
-      merchantToken: ['', Validators.required]
+      merchantToken: ['', Validators.required],
+      timezoneId: ['', Validators.required],
     });
   }
-
 }
