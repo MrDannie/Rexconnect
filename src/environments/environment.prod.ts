1f617c081de4e906b5217b87858ab866c956c56c
Afodunrinbi Olanrewaju <lanre.afod@globalaccelerex.com>
2021-03-01 16:18:25 +0000

    environment.prod.ts edited online with Bitbucket

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  BASE_URL: 'https://sandbox.globalaccelerex.com/api/rexconnect',
+  BASE_URL: 'https://rexconnect-backend.globalaccelerex.com/api/rexconnect',
   EXTERNAL_BASE_URL: 'https://pgs.globalaccelerex.com',
   APP_REF: 'REXCONNECT-',
   ACCESS_CONTROL_BTOA: btoa("rexconnect-ui-prod" + ":" + "Ze5bJ2sQ6KYdsWAM"),
