e69506f09a23561fcd3a8d48d208f146b65e101e
Rahman Hafeez <rahman.hafeez@globalaccelerex.com>
2021-03-01 10:53:37 +0100

    new production credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  BASE_URL: 'https://accesstams.globalaccelerex.com/api/tams',
+  BASE_URL: 'https://sandbox.globalaccelerex.com/api/rexconnect',
   EXTERNAL_BASE_URL: 'https://pgs.globalaccelerex.com',
   APP_REF: 'REXCONNECT-',
-  ACCESS_CONTROL_BTOA: btoa("peter" + ":" + "peter"),
+  ACCESS_CONTROL_BTOA: btoa("rexconnect-ui-prod" + ":" + "Ze5bJ2sQ6KYdsWAM"),
   NONCE: "TESTME",
   TIMESTAMP: "1670080359",
 };
