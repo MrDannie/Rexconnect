eb1526830c1930552619b3a8f159cc985bf89362
Daniel Agbogun <daniel.agbogun@gmail.com>
2021-06-04 11:13:40 +0100

    hotfix/RK-409-mask-pan-keys-in-acquire-mercht-mod

--- src/app/pages/user/user-layout/manage-acquirer/acquirer-merchants/acquirer-merchant-transactions/acquirer-merchant-transactions.component.ts
+++ src/app/pages/user/user-layout/manage-acquirer/acquirer-merchants/acquirer-merchant-transactions/acquirer-merchant-transactions.component.ts
@@ -9,11 +9,13 @@ import { ActivatedRoute, Params } from '@angular/router';
 // tslint:disable
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { MaskSensitiveKeys } from 'src/app/pages/shared/pipes/mask-sensitve-key.pipe';
 
 @Component({
   selector: 'app-acquirer-merchant-transactions',
   templateUrl: './acquirer-merchant-transactions.component.html',
   styleUrls: ['./acquirer-merchant-transactions.component.scss'],
+  providers: [MaskSensitiveKeys],
 })
 export class AcquirerMerchantTransactionsComponent implements OnInit {
   showFilter: boolean;
@@ -42,8 +44,8 @@ export class AcquirerMerchantTransactionsComponent implements OnInit {
     private merchants: MerchantsService,
     private paginationService: PaginationService,
     private alerts: AlertService,
-    private errorHandler: ErrorHandler,
-    private fileGenerationService: FileGenerationService
+    private fileGenerationService: FileGenerationService,
+    private maskSensitiveKeys: MaskSensitiveKeys
   ) {
     this.showFilter = false;
     this.initializeForm();
